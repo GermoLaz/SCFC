d245104ca4d4dc78954d6d11dc1dc8647ddb7732
Germán Lazcano <glazcan@mapfre.com.ar>
2023-11-14 20:23:04 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MainPageComponent,
+    FileUploadComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ src/app/components/file-upload/file-upload.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css'],
+})
+export class FileUploadComponent {
+  selectedFile: File | null = null;
+
+  onFileDropped(event: DragEvent) {
+    event.preventDefault();
+    if(event.dataTransfer){
+      this.extractFile(event.dataTransfer.files);
+    }
+  }
+
+  onFileSelected(event: any) {
+    const inputElement = event.target as HTMLInputElement;
+    this.extractFile(inputElement.files);
+  }
+
+  extractFile(files: FileList | null) {
+    if (files && files.length > 0) {
+      // Permitir solo un archivo, si necesitas más, ajusta según tus necesidades
+      const file = files[0];
+      if (this.isValidFile(file)) {
+        this.selectedFile = file;
+      } else {
+        console.error('Tipo de archivo no válido.');
+      }
+    }
+  }
+
+  isValidFile(file: File): boolean {
+    // Implementa tu lógica de validación de archivos aquí
+    // En este ejemplo, se aceptan archivos de imagen, PDF y documentos de Word
+    const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    return allowedTypes.includes(file.type);
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+}
